1f83b1c3cd178c86fc08af186e95044a471dad35
malone <minrui19830912@163.com>
2019-04-27 13:42:39 +0800

    add loading dialog

--- app/src/main/java/com/android/nejm/Fragments/BaseFragment.java
+++ app/src/main/java/com/android/nejm/Fragments/BaseFragment.java
@@ -1,6 +1,14 @@
 package com.android.nejm.Fragments;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.widgets.LoadingDialog;
 
 public class BaseFragment extends Fragment {
     protected Context mContext;
