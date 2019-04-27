4b1fe8a7807b3081e1eb37a318fd5907149e25ab
malone <minrui19830912@163.com>
2019-04-27 13:42:18 +0800

    add loading dialog

--- app/src/main/java/com/android/nejm/Fragments/BaseFragment.java
+++ app/src/main/java/com/android/nejm/Fragments/BaseFragment.java
@@ -3,4 +3,23 @@ package com.android.nejm.Fragments;
 import android.support.v4.app.Fragment;
 
 public class BaseFragment extends Fragment {
+    protected Context mContext;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mContext= getActivity();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        LoadingDialog.cancelDialogForLoading();
+    }
 }
---
+++ app/src/main/java/com/android/widgets/LoadingDialog.java
@@ -0,0 +1,65 @@
+package com.android.widgets;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.LinearLayout;
+
+import com.android.nejm.R;
+
+
+public class LoadingDialog {
+    /** 加载数据对话框 */
+    private static Dialog mLoadingDialog;
+    /**
+     * 显示加载对话框
+     * @param context 上下文
+     * @param msg 对话框显示内容
+     * @param cancelable 对话框是否可以取消
+     */
+    public static Dialog showDialogForLoading(Context context, String msg, boolean cancelable) {
+        View view = LayoutInflater.from(context).inflate(R.layout.dialog_loading, null);
+        View iv_ba_loading = view.findViewById(R.id.iv_ba_loading);
+        Animation operatingAnim = AnimationUtils.loadAnimation(context, R.anim.tip);
+        LinearInterpolator lin = new LinearInterpolator();
+        operatingAnim.setInterpolator(lin);
+        iv_ba_loading.startAnimation(operatingAnim);
+
+        mLoadingDialog = new Dialog(context, R.style.CustomProgressDialog);
+        mLoadingDialog.setCancelable(cancelable);
+        mLoadingDialog.setCanceledOnTouchOutside(false);
+        mLoadingDialog.setContentView(view, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        mLoadingDialog.show();
+        return  mLoadingDialog;
+    }
+
+    public static Dialog showDialogForLoading(Context context) {
+        View view = LayoutInflater.from(context).inflate(R.layout.dialog_loading, null);
+        View iv_ba_loading = view.findViewById(R.id.iv_ba_loading);
+        Animation operatingAnim = AnimationUtils.loadAnimation(context, R.anim.tip);
+        LinearInterpolator lin = new LinearInterpolator();
+        operatingAnim.setInterpolator(lin);
+        iv_ba_loading.startAnimation(operatingAnim);
+        cancelDialogForLoading();
+        mLoadingDialog = new Dialog(context, R.style.CustomProgressDialog);
+        mLoadingDialog.setCancelable(true);
+        mLoadingDialog.setCanceledOnTouchOutside(false);
+        mLoadingDialog.setContentView(view, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        mLoadingDialog.show();
+        return  mLoadingDialog;
+    }
+
+    /**
+     * 关闭加载对话框
+     */
+    public static void cancelDialogForLoading() {
+        if(mLoadingDialog != null && mLoadingDialog.isShowing()) {
+            mLoadingDialog.cancel();
+            mLoadingDialog=null;
+        }
+    }
+}
