944802426b3f933698a0d5d37aead887144ea5d9
malone <minrui19830912@163.com>
2019-05-01 10:20:36 +0800

    add back and title process api

--- app/src/main/java/com/android/nejm/activitys/BaseActivity.java
+++ app/src/main/java/com/android/nejm/activitys/BaseActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.nejm.R;
 
 public  class BaseActivity extends AppCompatActivity {
     protected Context mContext;
@@ -11,6 +15,21 @@ public  class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext= this;
+    }
+
+    protected void setTitle(String title){
+        ((TextView)findViewById(R.id.common_tile)).setText(title);
+    }
+
+    protected void showBack(){
+      View back = findViewById(R.id.iv_back);
+        back.setVisibility(View.VISIBLE);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
