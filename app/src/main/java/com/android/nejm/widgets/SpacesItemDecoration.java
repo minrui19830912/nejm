b2b1c324225de53d99f39af4977e68cbace8b67e
malone <minrui19830912@163.com>
2019-05-03 21:03:14 +0800

    home fragment code modify

---
+++ app/src/main/java/com/android/nejm/widgets/SpacesItemDecoration.java
@@ -0,0 +1,29 @@
+package com.android.nejm.widgets;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by malone on 2017/1/7.
+ */
+
+public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+    private int space;
+
+    public SpacesItemDecoration(int space) {
+        this.space = space;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view,
+                               RecyclerView parent, RecyclerView.State state) {
+        //outRect.left = space;
+        outRect.right = space;
+       // outRect.bottom = space;
+
+        // Add top margin only for the first item to avoid double space between items
+//        if (parent.getChildPosition(view) == 0)
+//            outRect.top = space;
+    }
+}
