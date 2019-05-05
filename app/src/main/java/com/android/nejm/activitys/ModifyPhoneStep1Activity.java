2a47307d38f6e54253404716c67fd235528b416a
bill20170929 <bill20170929@163.com>
2019-05-05 15:54:01 +0800

    commit modify phone ui and code

---
+++ app/src/main/java/com/android/nejm/activitys/ModifyPhoneStep1Activity.java
@@ -0,0 +1,36 @@
+package com.android.nejm.activitys;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.EditText;
+
+import com.android.nejm.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class ModifyPhoneStep1Activity extends BaseActivity {
+    @BindView(R.id.editTextOldPhone)
+    EditText editTextOldPhone;
+    @BindView(R.id.editTextVerifyCode)
+    EditText editTextVerifyCode;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_phone_step1);
+        ButterKnife.bind(this);
+        showBack();
+    }
+
+    @OnClick(R.id.textViewSendVerifyCode)
+    public void onClickSendVerifyCode() {
+
+    }
+
+    @OnClick(R.id.buttonNext)
+    public void onClickNext() {
+
+    }
+}
