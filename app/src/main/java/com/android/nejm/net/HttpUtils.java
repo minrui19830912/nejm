73a3aa09e99832c3316f7e89cf8f3eaf982f2cd2
bill20170929 <bill20170929@163.com>
2019-05-19 20:44:52 +0800

    fix bug

--- app/src/main/java/com/android/nejm/net/HttpUtils.java
+++ app/src/main/java/com/android/nejm/net/HttpUtils.java
@@ -364,10 +364,13 @@ String client_id =LoginUserManager.getInstance().client_id;
     public static void getReadRecordList(final Context context, int page, final OnNetResponseListener listener){
         long timeStamp= System.currentTimeMillis();
 
-        String sign= generateMd5Str("",timeStamp,APP_KEY,"");
+        String access_token = LoginUserManager.getInstance().access_token;
+        String client_id = LoginUserManager.getInstance().client_id;
+        String sign= generateMd5Str(access_token,timeStamp,APP_KEY,client_id);
         StringBuilder build=new StringBuilder("^");
 //access_token^timestamp^clientid
-        build.append("").append("^").append(timeStamp).append("^").append("");
+        build.append(access_token).append("^").append(timeStamp).append("^").append(client_id);
+
         StringBuilder url = new StringBuilder(READ_RECORD_URL);
         url.append("&page=").append(page);
         OkGo.get(url.toString()).headers("Authorization",sign+build.toString()).execute(new StringNetCallback(context) {
