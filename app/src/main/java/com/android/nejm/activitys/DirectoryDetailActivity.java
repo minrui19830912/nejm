d0701481faaa03b1e582a0b294e31afc479fd6c2
malone <minrui19830912@163.com>
2019-06-01 15:29:47 +0800

    update directory deatil

--- app/src/main/java/com/android/nejm/activitys/DirectoryDetailActivity.java
+++ app/src/main/java/com/android/nejm/activitys/DirectoryDetailActivity.java
@@ -2,26 +2,21 @@ package com.android.nejm.activitys;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Animatable;
-import android.net.Uri;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import com.android.nejm.R;
 import com.android.nejm.data.DirectoryBean;
+import com.android.nejm.net.Constant;
 import com.android.nejm.net.HttpUtils;
 import com.android.nejm.net.OnNetResponseListener;
 import com.android.nejm.widgets.LoadingDialog;
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.controller.BaseControllerListener;
-import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
-import com.facebook.imagepipeline.image.ImageInfo;
 import com.google.gson.Gson;
 
 import org.json.JSONObject;
@@ -38,8 +33,10 @@ public class DirectoryDetailActivity extends BaseActivity {
     TextView textViewDirName2;
     @BindView(R.id.textViewDate)
     TextView textViewDate;
-    @BindView(R.id.textViewContent)
-    TextView textViewContent;
+//    @BindView(R.id.textViewContent)
+//    TextView textViewContent;
+    @BindView(R.id.webview)
+    WebView mWebView;
 
     DirectoryBean directoryBean;
 
@@ -56,11 +53,24 @@ public class DirectoryDetailActivity extends BaseActivity {
         showBack();
         setCommonTitle("NEJM医学前沿");
         ButterKnife.bind(this);
-
+        initWebView();
         String id = getIntent().getStringExtra("id");
         loadData(id);
     }
+    private void initWebView() {
 
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.getSettings()
+                .setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings()
+                .setJavaScriptCanOpenWindowsAutomatically(true);
+    }
     private void loadData(String id) {
         LoadingDialog.showDialogForLoading(this);
         HttpUtils.getDirectoryDetails(this, id, new OnNetResponseListener() {
@@ -68,42 +78,92 @@ public class DirectoryDetailActivity extends BaseActivity {
             public void onNetDataResponse(JSONObject json) {
                 LoadingDialog.cancelDialogForLoading();
                 directoryBean = new Gson().fromJson(json.toString(), DirectoryBean.class);
-                setControllerListener(draweeViewLogo, directoryBean.logo);
+//                setControllerListener(draweeViewLogo, directoryBean.logo);
+                draweeViewLogo.setImageURI(directoryBean.logo);
                 textViewDirName.setText(directoryBean.item.title);
                 textViewDirName2.setText(directoryBean.item.stitle);
                 textViewDate.setText(directoryBean.item.postdate);
-                textViewContent.setText(Html.fromHtml(directoryBean.item.content));
+//                textViewContent.setText(Html.fromHtml(directoryBean.item.keyword));
+                mWebView.loadDataWithBaseURL(HttpUtils.BASE_URL, Constant.WEBVIEW_START + directoryBean.item.keyword + Constant.WEBVIEW_END, "text/html",  "utf-8", null);
             }
         });
     }
 
-    public static void setControllerListener(final SimpleDraweeView simpleDraweeView, String imagePath) {
-        final ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams)simpleDraweeView.getLayoutParams();
-        BaseControllerListener<ImageInfo> controllerListener = new BaseControllerListener<ImageInfo>() {
-            @Override
-            public void onFinalImageSet(String id, @Nullable ImageInfo imageInfo, @Nullable Animatable anim) {
-                if (imageInfo == null) {
-                    return;
+//    public static void setControllerListener(final SimpleDraweeView simpleDraweeView, String imagePath) {
+//        final ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams)simpleDraweeView.getLayoutParams();
+//        BaseControllerListener<ImageInfo> controllerListener = new BaseControllerListener<ImageInfo>() {
+//            @Override
+//            public void onFinalImageSet(String id, @Nullable ImageInfo imageInfo, @Nullable Animatable anim) {
+//                if (imageInfo == null) {
+//                    return;
+//                }
+//                int imageWidth = simpleDraweeView.getWidth();
+//                int height = imageInfo.getHeight();
+//                int width = imageInfo.getWidth();
+//                layoutParams.width = imageWidth;
+//                layoutParams.height = (int) ((float) (imageWidth * height) / (float) width);
+//                simpleDraweeView.setLayoutParams(layoutParams);
+//            }
+//
+//            @Override
+//            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
+//                Log.d("TAG", "Intermediate image received");
+//            }
+//
+//            @Override
+//            public void onFailure(String id, Throwable throwable) {
+//                throwable.printStackTrace();
+//            }
+//        };
+//        DraweeController controller = Fresco.newDraweeControllerBuilder().setControllerListener(controllerListener).setUri(Uri.parse(imagePath)).build();
+//        simpleDraweeView.setController(controller);
+//    }
+
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.e("NEJM", "shouldOverrideUrlLoading, url = " + url);
+            if(TextUtils.equals(url, "http://www.nejm.login/")) {
+                Intent intent = new Intent(mContext,LoginActivity.class);
+                intent.putExtra("just_finish",true);
+                startActivityForResult(intent,1001);
+                return true;
+            } else if (url.startsWith("http://www.nejm.com/?videoId=")) {
+                int length = "http://www.nejm.com/?videoId=".length();
+                if(url.length() > length) {
+                    String videoId = url.substring(length);
+                    VideoDetailActivity.launchActivity(mContext, videoId);
+                    return true;
                 }
-                int imageWidth = simpleDraweeView.getWidth();
-                int height = imageInfo.getHeight();
-                int width = imageInfo.getWidth();
-                layoutParams.width = imageWidth;
-                layoutParams.height = (int) ((float) (imageWidth * height) / (float) width);
-                simpleDraweeView.setLayoutParams(layoutParams);
+            } else if (url.startsWith("http://www.nejm.com/?classid=")) {
+                int length = "http://www.nejm.com/?classid=".length();
+                int index = url.indexOf('&');
+                if(url.length() > length && index > length) {
+                    String id = url.substring(length, index);
+                    SpecialFieldListActivity.launchActivity(mContext, "", id);
+                    return true;
+                }
             }
 
-            @Override
-            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
-                Log.d("TAG", "Intermediate image received");
-            }
+//            else if(url.startsWith("http://www.nejm.outlink")) {
+//                Intent intent= new Intent();
+//                intent.setAction("android.intent.action.VIEW");
+//                Uri content_url = Uri.parse(shareContent.outlink);
+//                intent.setData(content_url);
+//                startActivity(intent);
+//                return true;
+//            }
 
-            @Override
-            public void onFailure(String id, Throwable throwable) {
-                throwable.printStackTrace();
-            }
-        };
-        DraweeController controller = Fresco.newDraweeControllerBuilder().setControllerListener(controllerListener).setUri(Uri.parse(imagePath)).build();
-        simpleDraweeView.setController(controller);
+            return false;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+        }
+
     }
 }
